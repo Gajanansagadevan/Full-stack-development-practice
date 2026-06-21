b5690a872f775cd05b60c70c9ca60b5e4bcd6217
Gajanan <sagadevangajanan@gmail.com>
2026-06-21 20:33:41 +0530

    Calculator

---
+++ My Task/02. Java While loop/Task 10/MenuCalculator.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class MenuCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int choice = 0;
+
+        while (choice != 5) {
+
+            System.out.println("\n===== Calculator =====");
+            System.out.println("1. Addition");
+            System.out.println("2. Subtraction");
+            System.out.println("3. Multiplication");
+            System.out.println("4. Division");
+            System.out.println("5. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+
+            if (choice >= 1 && choice <= 4) {
+                System.out.print("Enter first number: ");
+                double num1 = sc.nextDouble();
+
+                System.out.print("Enter second number: ");
+                double num2 = sc.nextDouble();
+
+                switch (choice) {
+                    case 1:
+                        System.out.println("Result = " + (num1 + num2));
+                        break;
+
+                    case 2:
+                        System.out.println("Result = " + (num1 - num2));
+                        break;
+
+                    case 3:
+                        System.out.println("Result = " + (num1 * num2));
+                        break;
+
+                    case 4:
+                        if (num2 != 0) {
+                            System.out.println("Result = " + (num1 / num2));
+                        } else {
+                            System.out.println("Cannot divide by zero.");
+                        }
+                        break;
+                }
+
+            } else if (choice == 5) {
+                System.out.println("Exiting Calculator...");
+            } else {
+                System.out.println("Invalid choice.");
+            }
+        }
+
+        sc.close();
+    }
+}
