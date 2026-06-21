69de6c75b7765240fe37de6fa1d9f5e3443da827
Gajanan <sagadevangajanan@gmail.com>
2026-06-21 18:27:19 +0530

    Java Operators

---
+++ My Task/Java operators and contorl flow statements/Task 01/Multiplication.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Multiplication {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number (a): ");
+        int a = input.nextInt();
+        System.out.print("Enter second number (b): ");
+        int b = input.nextInt();
+
+        System.out.println("The result is: " +(a*b));
+        input.close();
+    }
+}
---
+++ My Task/Java operators and contorl flow statements/Task 01/Subtraction.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[]args){
+       Scanner input = new Scanner(System.in);
+       System.out.print("Enter first number (a)");
+       int a = input.nextInt();
+       System.out.print("Enter second number (b)");
+       int b = input.nextInt();
+
+       System.out.println("The result is: " + (a-b));
+
+       input.close();
+    }
+}
