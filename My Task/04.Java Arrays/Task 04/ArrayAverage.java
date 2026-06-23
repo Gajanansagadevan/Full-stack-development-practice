75dc37e7e47f7afdff069f543d2a542f230c7e49
Gajanan <sagadevangajanan@gmail.com>
2026-06-23 21:42:35 +0530

    Average Array

---
+++ My Task/04.Java Arrays/Task 04/ArrayAverage.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ArrayAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int num : array) {
+            sum += num;
+        }
+
+        double average = (double) sum / size;
+        System.out.println("The average of the array is: " + average);
+        scanner.close();
+    }
+}
