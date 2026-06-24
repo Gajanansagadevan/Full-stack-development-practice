51c3d1513d6b86d13ca4fd52098887480136f835
Gajanan <sagadevangajanan@gmail.com>
2026-06-24 14:30:18 +0530

    Rotate Right

---
+++ My Task/04.Java Arrays/Task 10/RotateRight.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class RotateRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        int lastElement = array[n - 1];
+        
+        for (int i = n - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        
+        array[0] = lastElement;
+        
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+        scanner.close();
+    }
+}
