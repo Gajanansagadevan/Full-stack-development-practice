2a9ea73692f6cc1e9e6de2258d2defb4a2d53e61
Gajanan <sagadevangajanan@gmail.com>
2026-06-24 13:13:12 +0530

    Even Odd Count

---
+++ My Task/04.Java Arrays/Task 05/EvenOddCount.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class EvenOddCount {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int num : array) {
+            if (num % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+
+        System.out.println("Number of even elements: " + evenCount);
+        System.out.println("Number of odd elements: " + oddCount);
+        scanner.close();
+    }
+}
