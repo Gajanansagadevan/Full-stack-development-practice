18c08ade04b902924f9c4b3f8cc0c5e78d53a668
Gajanan <sagadevangajanan@gmail.com>
2026-06-27 13:31:04 +0530

    Reverse word

---
+++ My Task/06.Java Arrays & Number Logic - Practical Tasks/Task 09/ReverseWord.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class ReverseWord {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = input.nextLine();
+
+        String[] words = sentence.split(" ");
+
+        System.out.print("Reversed Sentence: ");
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.print(words[i] + " ");
+        }
+    }
+}
